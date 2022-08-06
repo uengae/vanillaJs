6c1e437069f50427bffdbe51def883f4579c5209
uengae <humo0517@gmail.com>
2022-08-06 15:38:27 +0900

    add var

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const a=1;
 const b=1;
 let ab="abababa";
+var abc="123123";
 
 console.log(a);
 console.log(b);
 console.log(ab);
+console.log(abc);
 
 ab="babababa";
+abc="4242424242";
 
 console.log(a);
 console.log(b);
 console.log(ab);
+console.log(abc);
